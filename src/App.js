9b28bb1b504a83679e1675abf69547b10544c295
sanukumar123 <sanugupta4970@gmail.com>
2024-06-07 18:38:47 +0530

    Use React Router DOM

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import Home from './Home'
+import Home from './Home';
+import SingleMovie from './SingleMovie';
+import Error from './Error';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 const App = () => {
   return (
-    <Home></Home>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<Home />} />
+        <Route path = "movie/:id" element= {<SingleMovie />} />
+        <Route path= "*" element = {<Error/>} />
+      </Routes>
+    </BrowserRouter>
   )
 };
 
